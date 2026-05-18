1997c0ff93593fc04e396ffa085dc526d91a1830
Vinayak V P <156646641+vinayakvision@users.noreply.github.com>
2026-05-18 09:47:31 +0530

    Add files via upload

---
+++ sources_final_without_SRAM_Macro/genus_outputs/genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 20.11-s111_1, built Apr 26 2021 14:57:38
+
+# Date: Fri Apr 17 23:13:22 2026
+# Host: vvcevpn (x86_64 w/Linux 3.10.0-1160.el7.x86_64) (24cores*96cpus*2physical cpus*Intel(R) Xeon(R) Gold 5318N CPU @ 2.10GHz 36864KB)
+# OS:   CentOS Linux release 7.9.2009 (Core)
+
+source script.tcl
---
+++ sources_final_without_SRAM_Macro/genus_outputs/read_libs.tcl
@@ -0,0 +1,3 @@
+add_search_path ../library -library -both
+read_library -liberty -both /home/vlsi_S34/Desktop/temp1/work/work/../library/slow.lib /home/vlsi_S34/Desktop/temp1/work/work/../library/fast.lib
+
